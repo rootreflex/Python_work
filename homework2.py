d772a19e5040c8f16d3c9aa348da321167e8ebfe
rootreflex <reflex21@gmail.com>
2024-05-17 23:08:13 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,12 @@
+name = "Victor"
+print ("Имя:", name)
+
+age = 39
+print ("Возрост:", age)
+
+new_age = age + 1
+print ("Новый возрост:", new_age)
+
+in_student = True
+print ("in_student: ", in_student)
+
