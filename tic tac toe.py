60bfc72f818338435f0547764d3db12e08d504bf
rootreflex <reflex21@gmail.com>
2024-05-28 23:23:42 +0300

    Крестики нолики

---
+++ tic tac toe.py
@@ -0,0 +1,75 @@
+print('"Крестики нолики":')
+def chek_winner():
+    if area[0][0] == "X" and area[0][1] == "X" and area[0][2] == "X":
+        return "X"
+    if area[1][0] == "X" and area[1][1] == "X" and area[1][2] == "X":
+        return "X"
+    if area[2][0] == "X" and area[2][1] == "X" and area[2][2] == "X":
+        return "X"
+    if area[0][0] == "X" and area[1][0] == "X" and area[2][0] == "X":
+        return "X"
+    if area[0][1] == "X" and area[1][1] == "X" and area[2][1] == "X":
+        return "X"
+    if area[0][2] == "X" and area[1][2] == "X" and area[2][2] == "X":
+        return "X"
+    if area[0][0] == "X" and area[1][1] == "X" and area[2][2] == "X":
+        return "X"
+    if area[0][2] == "X" and area[2][1] == "X" and area[2][0] == "X":
+        return "X"
+
+    if area[0][0] == "0" and area[0][1] == "0" and area[0][2] == "0":
+        return "0"
+    if area[1][0] == "0" and area[1][1] == "0" and area[1][2] == "0":
+        return "X"
+    if area[2][0] == "0" and area[2][1] == "0" and area[2][2] == "0":
+        return "X"
+    if area[0][0] == "0" and area[1][0] == "0" and area[2][0] == "0":
+        return "X"
+    if area[0][1] == "0" and area[1][1] == "0" and area[2][1] == "0":
+        return "X"
+    if area[0][2] == "0" and area[1][2] == "0" and area[2][2] == "0":
+        return "X"
+    if area[0][0] == "0" and area[1][1] == "0" and area[2][2] == "0":
+        return "X"
+    if area[0][2] == "0" and area[2][1] == "0" and area[2][0] == "0":
+        return "0"
+def draw_area():
+    for i in area:
+        print(*i)
+    print()
+
+area = [["*", "*", "*"], ["*", "*", "*"], ["*", "*", "*"]]
+print("Добро пожаловать в крестики - нолики")
+print("______________________________________")
+draw_area()
+for turn in range(1,10):
+    print(f'Ход:{turn}')
+    if turn % 2 == 0:
+        turn_char = "0"
+        print("Ходят нолики")
+    else:
+        turn_char = "X"
+        print("Ходят крестики")
+    row = int(input("Введите номер строки (1,2,3) ")) - 1
+    column = int(input("Введите номер столбца (1,2,3) ")) - 1
+    if area [row][column] == "*":
+        area[row][column] = turn_char
+    else:
+        print("Ячейка уже занята, вы пропускаете ход")
+        draw_area()
+        continue
+
+    draw_area()
+
+    if chek_winner() =="X":
+        print("Победа крестиков")
+        break
+    if chek_winner() =="0":
+        print("Победа ноликов")
+        break
+    if chek_winner() =="*" and turn_char == 9:
+        print("Ничья")
+
+
+print('"Крестики нолики":')
+print("Created by VectorⓇ")
