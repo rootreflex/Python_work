d7a8e4e7e65f30018272de09de5f168526972560
rootreflex <reflex21@gmail.com>
2024-06-03 00:01:12 +0300

    Распаковка параметров и параметры функции

---
+++ module_2_6_1.py
@@ -0,0 +1,20 @@
+print('"Распаковка параметров и параметры функции"')
+print("")
+def print_params (a = 1, b = 'строка', c = False):
+   print(a,b,c)
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+Vector = 'Best repair service'
+values_list = [[1,2,3], 'Vector', True]
+values_dict = { 2024: 'Year', Vector : 'service',  True: False}
+values_list_2 = [bool, "football"]
+print(type(values_dict))
+print_params(*values_list)
+print_params(*values_dict)
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
+
+print("")
+print("")
+print("Created by VectorⓇ")
